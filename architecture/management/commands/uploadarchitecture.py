f8eaf55ec0a2f2e0828fea57a77ca0a661eb82ee
Aleksandar <arjkvc@gmail.com>
2025-04-14 10:44:44 +0100

    refactor architecture command

--- architecture/management/commands/uploadarchitecture.py
+++ architecture/management/commands/uploadarchitecture.py
@@ -1,42 +1,45 @@
-import csv
-import sys
 from django.core.management.base import BaseCommand, CommandError
-from django.db import transaction
-from architecture.models import Architecture
+from django.db import connection
 
-ARCHITECTURE_REQUIRED_COLUMNS = {"sequence_index", "accessions", "names", "score", "graphics"}
-
-csv.field_size_limit(sys.maxsize)
+ARCHITECTURE_REQUIRED_COLUMNS = {"sequence_index", "accessions", "names", "score", "graphics", "database"}
 
 
 class Command(BaseCommand):
     help = "Uploads the architecture from a CSV file"
 
     def add_arguments(self, parser):
-        parser.add_argument("architecture_file", type=str, help="the CSV file containing the architecture")
+        parser.add_argument("architecture_file", type=str, help="TSV file containing the architecture")
 
-        parser.add_argument("--replace", action="store_true", help="replace existing records")
+        parser.add_argument(
+            "--replace", action="store_true", help="replace existing records - THIS OPERATION IS DESTRUCTIVE"
+        )
+        parser.add_argument(
+            "--table", type=str, default="architecture_architecture", help="name of the architecture table"
+        )
 
     def handle(self, *args, **options):
-        architecture_file = options["architecture_file"]
+        with connection.cursor() as cursor, open(options["architecture_file"]) as tsv_fh:
+            if options["replace"]:
+                self.stdout.write(self.style.WARNING("Deleting existing architecture records..."))
+                cursor.execute(f"TRUNCATE {options["table"]} CASCADE;")
 
-        with open(architecture_file, newline="") as csvfile:
-            reader = csv.DictReader(csvfile)
-            header_set = set(reader.fieldnames)
+            self.stdout.write(f"Inserting rows into {self.style.SQL_TABLE(options["table"])}")
 
-            if not ARCHITECTURE_REQUIRED_COLUMNS.issubset(header_set):
-                missing = ARCHITECTURE_REQUIRED_COLUMNS - header_set
-                raise CommandError(f"CSV file {architecture_file} is missing required columns: {', '.join(missing)}")
+            header_line = tsv_fh.readline()
+            columns = list(map(str.strip, header_line.split("\t")))
+            columns_set = set(columns)
 
-            with transaction.atomic():
-                if options["replace"]:
-                    self.stdout.write(self.style.WARNING("Deleting existing architecture records..."))
-                    Architecture.objects.all().delete()
+            if not ARCHITECTURE_REQUIRED_COLUMNS.issubset(columns_set):
+                missing = ARCHITECTURE_REQUIRED_COLUMNS - columns_set
+                raise CommandError(
+                    f"TSV file {options["architecture_file"]} is missing required columns: {', '.join(missing)}"
+                )
 
-                self.stdout.write("Loading architecture records...")
+            if difference := columns_set.difference(ARCHITECTURE_REQUIRED_COLUMNS):
+                raise CommandError(
+                    f"TSV file {options["architecture_file"]} contains unknown columns: {', '.join(difference)}"
+                )
 
-                for row in reader:
-                    row["database"] = row["database"] if "database" in row else "uniprot"
-                    Architecture.objects.create(**row)
+            cursor.copy_from(tsv_fh, options["table"], sep="\t", columns=columns)
 
-            self.stdout.write(self.style.SUCCESS("Architecture records loaded successfully!"))
+            self.stdout.write(self.style.SUCCESS("Architecture loaded successfully!"))
