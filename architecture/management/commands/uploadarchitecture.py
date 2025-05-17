1e79cb8e810a0b944e3fe301e191c4f56f631fa0
Aleksandar <arjkvc@gmail.com>
2025-05-17 20:23:59 +0100

    refactored upload command

--- architecture/management/commands/uploadarchitecture.py
+++ architecture/management/commands/uploadarchitecture.py
@@ -1,8 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from django.contrib.postgres.indexes import HashIndex
 from django.db import connection
-
-from architecture.models import Architecture
 
 ARCHITECTURE_REQUIRED_COLUMNS = {"sequence_index", "accessions", "names", "score", "graphics", "database"}
 
@@ -21,25 +18,11 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        hash_index = next(
-            (idx for idx in Architecture._meta.indexes if isinstance(idx, HashIndex) and idx.fields == ["accessions"]),
-            None,
-        )
-
         with connection.cursor() as cursor, open(options["architecture_file"]) as tsv_fh:
             if options["replace"]:
                 self.stdout.write(self.style.WARNING("Deleting existing architecture records..."))
                 self.stdout.flush()
                 cursor.execute(f"TRUNCATE {options["table"]} CASCADE;")
-
-            if hash_index:
-                self.stdout.write(f"Found index on column 'accession': {hash_index.name}")
-                self.stdout.flush()
-
-                with connection.schema_editor() as schema_editor:
-                    self.stdout.write("Removing index on column 'accession'")
-                    self.stdout.flush()
-                    schema_editor.remove_index(Architecture, hash_index)
 
             self.stdout.write(f"Inserting rows into {self.style.SQL_TABLE(options["table"])}")
             self.stdout.flush()
@@ -48,8 +31,7 @@ class Command(BaseCommand):
             columns = list(map(str.strip, header_line.split("\t")))
             columns_set = set(columns)
 
-            if not ARCHITECTURE_REQUIRED_COLUMNS.issubset(columns_set):
-                missing = ARCHITECTURE_REQUIRED_COLUMNS - columns_set
+            if not bool(missing := ARCHITECTURE_REQUIRED_COLUMNS - columns_set):
                 raise CommandError(
                     f"TSV file {options["architecture_file"]} is missing required columns: {', '.join(missing)}"
                 )
@@ -63,9 +45,3 @@ class Command(BaseCommand):
 
             self.stdout.write(self.style.SUCCESS("Architecture loaded successfully!"))
             self.stdout.flush()
-
-            if hash_index:
-                with connection.schema_editor() as schema_editor:
-                    self.stdout.write("Adding index on column 'accession'")
-                    self.stdout.flush()
-                    schema_editor.add_index(Architecture, hash_index)
