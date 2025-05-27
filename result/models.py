d0a51c3d8144af9d6e89a488c4a8e74c6729f1a5
Aleksandar <arjkvc@gmail.com>
2025-05-27 08:09:27 +0100

    fix pfam metadata model

--- result/models.py
+++ result/models.py
@@ -830,7 +830,7 @@ def predict_active_sites(result: Result):
             subpattern = False
             matched_patterns = {}
 
-            for source, patterns in hit.metadata.active_site:
+            for source, patterns in hit.metadata.active_sites:
                 # Check this isn't a subpattern of a bigger pattern already found on the sequence
                 # Patterns were added to @{$self->{_act_site_data}} in order of size (longest pattern first)
                 for asp in matched_patterns.keys():
