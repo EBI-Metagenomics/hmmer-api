dce3d788dd163fa53a69c325bfdfdb461f5cc795
Aleksandar <arjkvc@gmail.com>
2025-07-28 15:24:13 +0100

    allow multiple databases

--- search/management/commands/database.py
+++ search/management/commands/database.py
@@ -7,25 +7,33 @@ class Command(BaseCommand):
     help = "Enables, disables or pauses searches for a database"
 
     def add_arguments(self, parser):
-        parser.add_argument("database", type=str)
         parser.add_argument("status", type=str, choices=["enable", "disable", "pause"])
+        parser.add_argument("database", type=str, nargs="+")
 
     def handle(self, *args, **options):
-        if options["database"] not in settings.HMMER.databases:
-            raise CommandError(f"Database '{options['database']}' is not supported")
+        for database in options["database"]:
+            if database not in settings.HMMER.databases:
+                raise CommandError(f"Database '{database}' is not supported")
 
-        try:
-            database = Database.objects.get(id=options["database"])
-        except Database.DoesNotExist:
-            raise CommandError(f"Entry for database '{options['database']}' not found")
+        databases = Database.objects.filter(id__in=options["database"])
+
+        missing_databases = set(options["database"]) - set([database.id for database in databases])
+
+        if missing_databases:
+            raise CommandError(f"Entries for databases {", ".join(missing_databases)} not found")
 
         if options["status"] == "enable":
-            database.status = Database.StatusChoices.ENABLED
+            status_to_set = Database.StatusChoices.ENABLED
         elif options["status"] == "disable":
-            database.status = Database.StatusChoices.DISABLED
+            status_to_set = Database.StatusChoices.DISABLED
         elif options["status"] == "pause":
-            database.status = Database.StatusChoices.PAUSED
+            status_to_set = Database.StatusChoices.PAUSED
+        else:
+            status_to_set = Database.StatusChoices.ENABLED
 
-        database.save()
+        for database in databases:
+            database.status = status_to_set
 
-        self.stdout.write(f"Searches for database '{database.id}' are {database.status}")
+        Database.objects.bulk_update(databases, ["status"])
+
+        self.stdout.write(f"Searches for databases {", ".join(options["database"])} are {status_to_set.value}")
